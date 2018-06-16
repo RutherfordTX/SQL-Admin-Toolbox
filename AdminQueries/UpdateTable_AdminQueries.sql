fb8fa07f8daed0093e128aa667ce18122256e07f
John Rutherford <40303174+RutherfordTX@users.noreply.github.com>
2018-06-15 20:06:03 -0500

    Changed query to show backups for last 7 days

--- AdminQueries/UpdateTable_AdminQueries.sql
+++ AdminQueries/UpdateTable_AdminQueries.sql
@@ -1,53 +1,85 @@
-use Admin
+USE Admin
 GO
-
 
 -- View the current contents
 SELECT *
 FROM dbo.AdminQueries
-WHERE name = 'BackupSettings'
+WHERE name = 'BackupsLastSevenDays'
 
 
 -- Create a row
 INSERT INTO dbo.AdminQueries
         ( name, description, code )
-VALUES  ( 'ClearBackupSettings', -- name - varchar(50)
-          'Clears the in progress flags for Full, BAK and Diff backups', -- description - varchar(200)
-          'UPDATE admin.dbo.tblBackupsDatabaseInfo
-SET FullBackupInProgress = NULL,
-BakBackupInProgress = NULL,
-DiffBackupInProgress = NULL'
+VALUES  ( 'BackupsLastSevenDays', -- name - varchar(50)
+          'Lists all Backups on a server for the previous 7 days', -- description - varchar(200)
+          'SELECT 
+   CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server,
+   bs.user_name,
+   bs.database_name,
+   bs.backup_start_date, 
+   bs.backup_finish_date,
+   bs.expiration_date,
+   CASE bs.type 
+       WHEN ''D'' THEN ''DB Full''
+       WHEN ''L'' THEN ''Log''
+       WHEN ''I'' THEN ''Differential''
+       ELSE bs.type
+   END AS backup_type,
+   bs.is_snapshot,
+   bs.is_copy_only,
+   bs.backup_size, 
+   bmf.logical_device_name, 
+   bmf.physical_device_name,  
+   bs.name AS backupset_name,
+ --  bs.description,
+   bs.first_lsn,
+   bs.last_lsn,
+   bs.database_backup_lsn,
+   bs.checkpoint_lsn
+FROM   msdb.dbo.backupmediafamily AS bmf
+INNER JOIN msdb.dbo.backupset AS bs
+    ON bmf.media_set_id = bs.media_set_id
+WHERE  (CONVERT(datetime, bs.backup_start_date, 102) >= GETDATE() - 7)
+ORDER BY 
+   bs.database_name,
+   bs.backup_finish_date DESC'
           )
 
 
 -- Update a row
 UPDATE dbo.adminqueries
-SET code = 'SELECT DatabaseName,
-	RecoveryModel,
-	DBStatus,
-	BakBackupNeeded,
-	BakBackupFrequency,
-	BakBackedUp,
-	BakBackupInProgress,
-	FullBackupNeeded,
-	FullBackupFrequency,
-	FullBackedUp,
-	FullBackupInProgress,
-	DiffBackupNeeded,
-	DiffBackupFrequency,
-	DiffBackedUp,
-	DiffBackupInProgress,
-	TLogBackupNeeded,
-	TLogBackupFrequency,
-	TLogBackedUp,
-	TLogBackupInProgress
-FROM dbo.tblBackupsDatabaseInfo
-WHERE DBStatus = ''Online''
-ORDER BY DatabaseName'
-WHERE name = 'BackupSettings'
+SET code = 'SELECT 
+   CONVERT(CHAR(100), SERVERPROPERTY('Servername')) AS Server,
+   bs.user_name,
+   bs.database_name,
+   bs.backup_start_date, 
+   bs.backup_finish_date,
+   bs.expiration_date,
+   CASE bs.type 
+       WHEN ''D'' THEN ''DB Full''
+       WHEN ''L'' THEN ''Log''
+       WHEN ''I'' THEN ''Differential''
+       ELSE bs.type
+   END AS backup_type,
+   bs.is_snapshot,
+   bs.is_copy_only,
+   bs.backup_size, 
+   bmf.logical_device_name, 
+   bmf.physical_device_name,  
+   bs.name AS backupset_name,
+ --  bs.description,
+   bs.first_lsn,
+   bs.last_lsn,
+   bs.database_backup_lsn,
+   bs.checkpoint_lsn
+FROM   msdb.dbo.backupmediafamily AS bmf
+INNER JOIN msdb.dbo.backupset AS bs
+    ON bmf.media_set_id = bs.media_set_id
+WHERE  (CONVERT(datetime, bs.backup_start_date, 102) >= GETDATE() - 7)
+ORDER BY 
+   bs.database_name,
+   bs.backup_finish_date DESC'
+WHERE name = 'BackupsLastSevenDays'
 
 
-EXEC admin.dbo.usp_RunAdminQuery @name='BackupSettings'
-
-EXEC dbaadmin.dbo.usp_UpdateAdminQueries
 
